6c9757ad6e9dcd84bdae14290fa487c90f138338
tgibbons-css <tgibbons@css.edu>
2017-02-03 09:57:33 -0600

    Setting up initial GUI.

--- app/src/main/java/css/cis3334/tipcalc2017/MainActivity.java
+++ app/src/main/java/css/cis3334/tipcalc2017/MainActivity.java
@@ -2,12 +2,17 @@ package css.cis3334.tipcalc2017;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etBill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etBill = (EditText) findViewById(R.id.editTextBill);
     }
 }
